b003f6f1e7ee32711e08d4dc75baadcbf536a4f5
N Kaushik <kaushiknishchay@gmail.com>
2018-04-03 14:38:54 +0530

    refactor(Header.jsx): Cleaner code to show the Sign In & Sign Out Button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Header(props) {
+export default function Header({ isSignIn, onClick }) {
   return (
     <nav className="navbar navbar-dark bg-dark">
       <a className="navbar-brand" href="/">React GitHub API</a>
       <form className="form-inline">
-        {
-                        !props.isSignIn &&
-                        <button
-                          className="btn btn-md"
-                          type="button"
-                          onClick={props.onClick}
-                        >
-                            Sign In
-                        </button>
-                    }
-        {
-                        props.isSignIn &&
-                        <button
-                          className="btn btn-md"
-                          type="button"
-                          onClick={props.onClick}
-                        >
-                            Sign Out
-                        </button>
-                    }
+        <button
+          className="btn btn-md"
+          type="button"
+          onClick={onClick}
+        >
+          {isSignIn ? 'Sign In' : 'Sign Out'}
+        </button>
+
       </form>
     </nav>
   );
