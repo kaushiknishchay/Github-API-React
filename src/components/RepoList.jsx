2a9be5a03d3d23118d2f5829ed2e3e5ec45becfe
N Kaushik <kaushiknishchay@gmail.com>
2018-04-03 14:34:51 +0530

    refactor(RepoList): Change the component into a functional component

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function RepoList(props) {
-  return (
-    <div>
-      <ul className="list-group">
-        {
-            props.data && props.data.map(repo => (
+const RepoList = ({ data }) => (
+  <div>
+    <ul className="list-group">
+      {
+            data && data.map(repo => (
               <li className="list-group-item" key={repo.id}>
                 {repo.name}
               </li>
             ))
         }
-      </ul>
-    </div>
-  );
-}
+    </ul>
+  </div>
+);
 
 
 RepoList.propTypes = {
@@ -23,3 +21,4 @@ RepoList.propTypes = {
   data: PropTypes.array.isRequired,
 };
 
+export default RepoList;
