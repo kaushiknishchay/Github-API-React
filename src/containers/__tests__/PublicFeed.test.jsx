7c726f198ea668bce47e029cd77e25a0ea53b807
N Kaushik <kaushiknishchay@gmail.com>
2018-04-10 12:41:47 +0530

    test(PublicFeed): test cases written for getMorePublicFeeds function

--- src/containers/__tests__/PublicFeed.test.jsx
+++ src/containers/__tests__/PublicFeed.test.jsx
@@ -12,15 +12,21 @@ describe('<PublicFeedC />', () => {
   it('should render', async () => {
     jest.useFakeTimers();
 
-    const mockGetPublicFeedfn = jest.spyOn(PublicFeedC.prototype, 'getPublicFeed');
+    const spyGetPublicFeedfn = jest.spyOn(PublicFeedC.prototype, 'getPublicFeed');
 
     const wrapper = mount(<PublicFeedC />);
 
-    expect(mockGetPublicFeedfn).toHaveBeenCalled();
+    // check if API triggered on component Mount
+    expect(spyGetPublicFeedfn).toHaveBeenCalled();
 
+    // check if FeedList component present
     expect(wrapper.find(FeedList)).toBeTruthy();
 
-    wrapper.instance().getPublicFeed(2);
+    // manually call API call to fetch more feeds
+    wrapper.instance().getMorePublicFeeds();
+
+    // check if getMorePublicFeeds triggered api call
+    expect(spyGetPublicFeedfn).toHaveBeenCalledWith(wrapper.state().publicFeedPageNum);
 
     await setTimeout(() => {
       expect(wrapper.state().feedList.length).toEqual(2);
